10cd44d9b1e370c883ed2e8d7d5ed7cb332116b8
Anit <anit7795@gmail.com>
2025-07-13 22:44:30 +0530

    campground update setup

--- app.js
+++ app.js
@@ -51,7 +51,10 @@ app.get('/campgrounds/:id', async(req,res) => {
     res.render('campgrounds/show', {campground})
 })
 
-
+app.get('/campgrounds/:id/edit', async (req,res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground})
+})
 
 app.listen(3000, () => {
     console.log('Listening at port 3000')
