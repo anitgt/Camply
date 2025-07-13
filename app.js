8fe6b28c2f101c78f352bb3702cf669546e2fefb
Anit <anit7795@gmail.com>
2025-07-13 22:30:12 +0530

    New Campground

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ mongoose.connect('mongodb://localhost:27017/Camply')
 const app = express();
 
 
-
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req,res) => {
     res.send('On')
@@ -33,10 +35,23 @@ app.get('/campgrounds', async (req,res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
+
+app.get('/campgrounds/new', (req,res) => {
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async(req,res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`campgrounds/${campground.id}`)
+})
+
 app.get('/campgrounds/:id', async(req,res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground})
 })
+
+
 
 app.listen(3000, () => {
     console.log('Listening at port 3000')
