2420a715b2815bd86d5174ae217b91e285e862de
Anit <anit7795@gmail.com>
2025-07-15 21:05:34 +0530

    ejs tool for styling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose')
-const path = require('path')
+const path = require('path');
+const ejsMate = require('ejs-mate')
 const Campground = require('./models/campground')
 const methodOverride = require('method-override')
 
@@ -15,7 +16,7 @@ mongoose.connect('mongodb://localhost:27017/Camply')
 
 const app = express();
 
-
+app.engine('ejs', ejsMate)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
