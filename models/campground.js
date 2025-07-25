e143db79aa30cfdf8bbf82fe8a544b60697d9d78
Anit <anit7795@gmail.com>
2025-07-25 21:05:13 +0530

    Delete Middleware

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -9,10 +10,19 @@ const CampgroundSchema = new Schema({
     location: String,
     reviews: [
         {
-        type: Schema.Types.ObjectId,
-        ref: 'Review'
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
         }
     ]
 });
 
-module.exports = mongoose.model('Campground', CampgroundSchema)
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: { $in: doc.reviews }
+        });
+    }
+});
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
