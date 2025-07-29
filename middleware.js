52f8768b9bee76c15b8bdb40282898fc8fb0c06a
Anit <anit7795@gmail.com>
2025-07-29 21:24:22 +0530

    return to url behaviour after loggin in

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 const isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl
         req.flash('error', 'You must be signed in!');
         return res.redirect('/login');
     }
@@ -7,3 +8,10 @@ const isLoggedIn = (req, res, next) => {
 };
 
 module.exports = { isLoggedIn };
+
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
