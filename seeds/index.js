b1c6b45e7f9e696ec37daac1b24d0c2553378dd3
Anit <anit7795@gmail.com>
2025-09-03 00:25:04 +0530

    fixing seeding bug

--- seeds/index.js
+++ seeds/index.js
@@ -4,26 +4,28 @@ const { places, descriptors } = require('./seedHeplers')
 const cities = require('../seeds/cities')
 
 mongoose.connect('mongodb://localhost:27017/Camply')
-    .then(()=> {
-        console.log('Connection Succesful')
+    .then(() => {
+        console.log('Connection Successful')
     })
-    .catch((err)=> {
-        console.log('Connection Failed'),
+    .catch((err) => {
+        console.log('Connection Failed')
         console.log(err)
     })
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)]
 
-const seedDB = async() => {
+const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i <= 50; i++) {
-        const random1000 = Math.floor(Math.random() *1000);
-        const price = Math.floor(Math.random() *20) + 10;
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '6888878231cfb761207e925b',
+            author: '6888878231cfb761207e925b', // replace with a real user _id
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, placeat at voluptatum sint vel impedit accusantium hic eligendi unde commodi possimus ullam tenetur! Assumenda ea sunt nesciunt atque, similique quibusdam?',
+            price,
+            geometry: { type: "Point", coordinates: [-113.3, 47.0202] }, // ✅ fixed
             images: [
                 {
                     url: `https://picsum.photos/400?random=${Math.random()}`,
