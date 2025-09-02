deb58ff5505d843f74f1e1eaeeb735b1b68ae8bf
Anit <anit7795@gmail.com>
2025-09-02 19:02:58 +0530

    Geocoding the locations

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,7 +1,8 @@
-const campground = require('../models/campground');
 const Campground = require('../models/campground');
-const { cloudinary } = require("../cloudinary")
+const maptilerClient = require("@maptiler/client");
+maptilerClient.config.apiKey = process.env.MAPTILER_API_KEY;
 
+const { cloudinary } = require("../cloudinary")
 module.exports.index =async (req,res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
@@ -12,7 +13,9 @@ module.exports.renderNewForm = (req,res) => {
 };
 
 module.exports.createCampground = async(req,res,next) => {
+const getData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 });
     const campground = new Campground(req.body.campground);
+    campground.geometry = getData.features[0].geometry;
     campground.images = req.files.map(f => ({url: f.path, filename: f.filename}))
     campground.author = req.user._id;
     await campground.save();
