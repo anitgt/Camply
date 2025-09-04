9ebce101e57a438d767cd06fa26f8b20bd4c7d41
Anit <anit7795@gmail.com>
2025-09-04 17:01:42 +0530

    installing express 4

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 
-const User = require('./models/user.js')
+const User = require('./models/user.js');
+
+const mongoSanitize = require('express-mongo-sanitize');
+
 
 const userRoutes = require('./routes/users.js')
 const campgroundRoutes = require('./routes/campgrounds.js');
@@ -41,6 +44,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(mongoSanitize())
 
 const sessionConfig = {
     secret: 'secret1',
@@ -64,7 +68,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next) => {
-    console.log(req.session)
+    console.log(req.query)
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error')
@@ -79,7 +83,7 @@ app.use('/campgrounds/:id/reviews', reviewRoutes);
 //     res.send('On')
 // })
 
-app.get('/fakeUser', async(rq,res) => {
+app.get('/fakeUser', async(req,res) => {
     const user = new User({email: 'anit7795@gmail.com', username: 'anitt'});
     const newUser = await User.register(user, 'monkey');
     res.send(newUser)
